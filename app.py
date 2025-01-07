2c8e041770390eea933880400c4a38ec57563743
nirmalvarbabu <113340320+nirmalvarbabu@users.noreply.github.com>
2025-01-07 10:14:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! Your AWS CI/CD setup is working!"
+    return "Hello! Your AWS setup is working!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
